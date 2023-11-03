e7f310e838488feaad5f1afde58504f232718e8c
Andylmao <luis.andres.garcia.lopez@uabc.edu.mx>
2023-11-03 16:54:06 -0700

    first commit

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Navigation from './components/Navigation';
+import NotesList from './components/NotesList';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
+//asda
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <div className="container p-4">
+        <Routes>
+          <Route path="/" element={<NotesList />} />
+          <Route path="/edit/:id" element={<CreateNote />} />
+          <Route path="/create" element={<CreateNote />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
+
