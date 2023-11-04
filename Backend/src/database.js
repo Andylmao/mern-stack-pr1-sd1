5f10916205ad60833065dd3fc641d353a91f3cb7
Andylmao <luis.andres.garcia.lopez@uabc.edu.mx>
2023-11-03 19:53:07 -0700

    update5

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -1,7 +1,17 @@
+
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://luisandresgarcialopez:p0zbCkeDX0c8sqoe@notesapp.nioquxi.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(uri).then(db => console.log('db is conected')).catch(err => console.log(err));
+const URI = process.env.MONGODB_URI
+    ? process.env.MONGODB_URI
+    : 'mongodb://localhost/databasetest';
 
+mongoose.connect(URI,{
+    useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    
+});
 
-module.exports = mongoose;
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('DB is connected');
+});
