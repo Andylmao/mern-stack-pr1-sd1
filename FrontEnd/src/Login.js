359ea158496be4abbd0211a6a329bd5a6c0b387b
Andylmao <luis.andres.garcia.lopez@uabc.edu.mx>
2023-11-15 00:32:10 -0800

    Se agrega sistema de login y registro 02

--- FrontEnd/src/Login.js
+++ FrontEnd/src/Login.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import axios from 'axios'
 
 function Login() {
     const[email,setEmail] = useState()
