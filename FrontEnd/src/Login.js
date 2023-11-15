b22b3b3c66530dd094ff8aa48144a9bf0f7b6b1a
Andylmao <luis.andres.garcia.lopez@uabc.edu.mx>
2023-11-15 00:55:00 -0800

    Se agrega sistema de login y registro 09

--- FrontEnd/src/Login.js
+++ FrontEnd/src/Login.js
@@ -25,7 +25,7 @@ function Login() {
     return(
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
             <div className="bg-white p-3 rounded w-25">
-                <h2> Login </h2>
+                <h2> Login! </h2>
                 <form onSubmit={handleSubmite}>
                     <div className="mb-3">
                         <label htmlFor="email">
