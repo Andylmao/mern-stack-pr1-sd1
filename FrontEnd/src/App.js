5f72a8496a88f00f8524d4caaaa75f427bb9d17b
Andylmao <luis.andres.garcia.lopez@uabc.edu.mx>
2023-11-15 18:18:58 -0800

    Se agrega sistema de login y registro Final

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,44 +1,39 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom';
-import './App.css';
-import Navigation from './components/Navigation';
-import NotesList from './components/NotesList';
-import CreateNote from './components/CreateNote';
-import CreateUser from './components/CreateUser';
-import Signup from './signup';
-import Login from './Login';
-import Home from './home';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import Login from './components/Login'
+import Notes from './components/Notes'
 
-//asda
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false)
 
-  return(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Signup/>}> </Route>
-        <Route path='/login' element={<Login/>}> </Route>
-        <Route path="/home" element={<Home/>}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-  /*
+  useEffect(() =>{
+    const checkLogin = async () =>{
+      const token = localStorage.getItem('tokenStore')
+      if(token){
+        const verified = await axios.get('/users/verify',{
+          headers:{ Authorization: token}
+        })
+        console.log(verified)
+        setIsLogin(verified.data)
+        if(verified.data === false) return localStorage.clear()
+      }else{
+        setIsLogin(false)
+      }
+    }
+    checkLogin()
+  }, [])
+
   return (
-    <Router>
-      <Navigation />
-      <div className="container p-4">
-        <Routes>
-          <Route path="/" element={<NotesList />} />
-          <Route path="/edit/:id" element={<CreateNote />} />
-          <Route path="/create" element={<CreateNote />} />
-          <Route path="/user" element={<CreateUser />} />
-        </Routes>
-      </div>
-    </Router>
-  );*/
+    <div className="App">
+      {
+        isLogin 
+        ? <Notes setIsLogin={setIsLogin} /> 
+        : <Login setIsLogin={setIsLogin} />
+      }
+    </div>
+  );
 }
-
 
 export default App;
 
