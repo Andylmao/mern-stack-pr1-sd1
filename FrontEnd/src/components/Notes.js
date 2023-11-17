8362e99594633a4c607a402550b264c6045717a9
Andylmao <luis.andres.garcia.lopez@uabc.edu.mx>
2023-11-16 22:10:48 -0800

    Se actualiza la DB 4

--- FrontEnd/src/components/Notes.js
+++ FrontEnd/src/components/Notes.js
@@ -3,7 +3,7 @@ import Header from './notes/Nav'
 import Home from './notes/Home'
 import CreateNote from './notes/CreateNote'
 import EditNote from './notes/EditNote'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 
 export default function Notes({setIsLogin}) {
     return (
@@ -11,9 +11,11 @@ export default function Notes({setIsLogin}) {
         <div className="notes-page">
             <Header setIsLogin={setIsLogin} />
             <section>
+                <Routes>
                 <Route path="/" component={Home} exact />
                 <Route path="/create" component={CreateNote} exact />
                 <Route path="/edit/:id" component={EditNote} exact />
+                </Routes>
             </section>
             
         </div>
